49b613bfb381e17f7e836e178bbe503ff1b6267c
siravit <mai_za_2010@hotmail.com>
2021-06-18 23:47:04 +0700

    update

--- test/custom_tab_controller_test.dart
+++ test/custom_tab_controller_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_avalant_test/controller/custom_tab_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,9 +5,12 @@ void main() {
   test('CustomTabController Test', () {
     final controller = CustomTabController();
     controller.activeIndex;
+    print('set activeIndex 0');
     expect(controller.activeIndex, 0);
+    print('activeIndex = 0');
     controller.activeIndex = 2;
+    print('set activeIndex 2');
     expect(controller.activeIndex, 2);
+    print('activeIndex = 2');
   });
-
 }
